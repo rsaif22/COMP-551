91f7dfa6304c98670d700887004e448033a74d2a
Rafid Saif <73025878+rsaif22@users.noreply.github.com>
2023-10-22 22:12:07 -0400

    MLP not working yet

--- Assignment_2/mlp.py
+++ Assignment_2/mlp.py
@@ -5,21 +5,22 @@ from node import *
 # it should initialize the weights and biases (with an
 class MLP:
     def __init__(self, activation_func: str, n_hidden_layers: int, hidden_units: list,
-                  input_dims: int, output_dims: int):
+                  input_dims: int, output_dims: int, initialization: str):
         self.activation_func = activation_func
         self.n_hidden = n_hidden_layers
         self.hidden_units = hidden_units
         self.input_dims = input_dims
         self.output_dims = output_dims
         self.num_classes = output_dims
+        self.initialization = initialization
         self.weights = []
         self.nodes = []
         # First weight
-        self.init_weights(initiailization="zeros")
+        self.init_weights()
         self.init_nodes(activation_func = activation_func)
 
-    def init_weights(self, initiailization: str = "zeros"):
-        if initiailization == "zeros":
+    def init_weights(self):
+        if self.initialization == "zeros":
             w_in = np.zeros((self.input_dims + 1, self.hidden_units))
             self.weights.append(w_in)
             # For all layers except last layer, weights between hidden layers
@@ -28,6 +29,26 @@ class MLP:
                 self.weights.append(w)
             # For output
             w_out = np.zeros((self.hidden_units + 1, self.output_dims))
+            self.weights.append(w_out)
+        if self.initialization == "ones":
+            w_in = np.ones((self.input_dims + 1, self.hidden_units))
+            self.weights.append(w_in)
+            # For all layers except last layer, weights between hidden layers
+            w = np.ones((self.hidden_units + 1, self.hidden_units)) # For all hidden layers
+            for i in range(self.n_hidden - 1):
+                self.weights.append(w)
+            # For output
+            w_out = np.ones((self.hidden_units + 1, self.output_dims))
+            self.weights.append(w_out)
+        if self.initialization == "uniform":
+            w_in = np.random.uniform(0, 1, (self.input_dims + 1, self.hidden_units))
+            self.weights.append(w_in)
+            # For all layers except last layer, weights between hidden layers
+            w = np.random.uniform(0, 1, (self.hidden_units + 1, self.hidden_units)) # For all hidden layers
+            for i in range(self.n_hidden - 1):
+                self.weights.append(w)
+            # For output
+            w_out = np.random.uniform(0, 1, (self.hidden_units + 1, self.output_dims))
             self.weights.append(w_out)
 
     def init_nodes(self, activation_func: Node):
@@ -56,10 +77,18 @@ class MLP:
     
     def compute_gradients(self, true_y: np.ndarray):
         weight_grads = []
-        d_out = self.nodes[-1].backward(true_y)
-        # for node in self.nodes:
-        #     if type(node) == Multiply:
-
+        d_out = self.nodes[-1].backward(true_y) # initial output gradient (10 X 10)
+        reversed_nodes = self.nodes.copy()
+        reversed_nodes.reverse()
+        for node in reversed_nodes:
+            if type(node) == Multiply:
+                d_out, weight_grad = node.backward(d_out)
+                weight_grads.append(weight_grad)
+            elif type(node) == ReLu:
+                d_out = node.backward(d_out)
+        weight_grads.reverse() # Flip to get right order
+        return weight_grads
+    
     def encode_y(self, y: np.ndarray):
         y = np.reshape(y, (y.size, 1))
         encoded_y = np.broadcast_to(y, (y.size, self.num_classes)).copy()
@@ -67,12 +96,35 @@ class MLP:
             class_num = i # Zero indexed
             encoded_y[:, i] = encoded_y[:, i] == class_num
         return encoded_y
+    
+    def fit(self, X: np.ndarray, y: np.ndarray, learning_rate: float = 0.01, epsilon: float = 1e-4,
+            batch_size:int = 10, max_iters:int = 1e3):
+        # self.f1_list = np.empty((3, 0))
+        # self.time_list = np.empty((0,))
+        y_encoded = self.encode_y(y)
+        all_indices = np.arange(y_encoded.shape[0])
+        current_batch = np.random.choice(all_indices, batch_size)
+        # forward pass
+        y_hat = self.forward(X[current_batch, :])
+        weight_grads = self.compute_gradients(y_encoded[current_batch, :])
+        num_iters = 0
+        # time_start = time.time()
+        while(num_iters < max_iters):
+            # self.f1_list = np.column_stack((self.f1_list, self.compute_F1(X, y)))
+            # time_from_start = time.time() - time_start
+            # self.time_list = np.append(self.time_list, time_from_start)
+            # self.w = self.w - learning_rate*grad
+            for i in range(len(self.weights)):
+                self.weights[i] = self.weights[i] - learning_rate * weight_grads[i]
+            current_batch = np.random.choice(all_indices, batch_size, replace=False)
+            y_hat = self.forward(X[current_batch, :])
+            weight_grads = self.compute_gradients(y_encoded[current_batch, :])
+            num_iters += 1
+        print("Finished")
 
     
 if __name__=="__main__":
-    mlp = MLP("ReLu", 2, 64, 748, 9)
-    # for weights in mlp.weights:
-    #     print(weights.shape)
-    for node in mlp.nodes:
-        print(type(node))
+    mlp = MLP("ReLu", 2, 16, 100, 5)
+    input = np.ones((100, 1))
+
     
--- Assignment_2/node.py
+++ Assignment_2/node.py
@@ -20,12 +20,16 @@ class Multiply(Node):
         if add_bias:
             self.input = np.append(self.input, np.ones((self.input.shape[0], 1)), axis=1)
         self.weights = weights
-        return self.input @ self.weights
+        N = weights.shape[0]
+        return (self.input @ self.weights) / N
     
     def backward(self, output_grad):
         local_grad_inputs = self.weights 
         local_grad_weights = self.input
-        return local_grad_inputs @ output_grad, local_grad_weights @ output_grad.T
+        input_grad = output_grad @ local_grad_inputs.T
+        input_grad = np.delete(input_grad, -1, axis=1)
+        weight_grad = local_grad_weights.T @ output_grad
+        return input_grad, weight_grad
     
 class ReLu(Node):
     def __init__(self):
@@ -46,30 +50,42 @@ class Softmax(Node):
 
     def forward(self, input: np.ndarray):
         self.input = input 
-        input_exp = np.exp(input)
-        exp_sum = np.sum(input_exp)
+        input_shifted = input.copy()
+        # for i in range(input.shape[1]):
+        #     input_shifted[:, i] = input[:, i] - np.max(input, 1)
+        max_values = np.max(input, 1).reshape((input.shape[0], 1))
+        input_shifted -= max_values
+        input_exp = np.exp(input_shifted)
+        exp_sum = np.sum(input_exp, axis=1).reshape((input_exp.shape[0], 1))
         self.output = input_exp / exp_sum # Store this as we will use this in gradient
         return self.output 
     
     def backward(self, output_true: np.ndarray): # As this is final layer, we only need the true y values
         N = np.size(output_true) # Size of output
-        return 1/N * self.input.T @ (self.output - output_true) # Gives derivative of loss directly
+        return 1/N * self.input * (self.output - output_true) # Gives derivative of loss directly
     
 
 if __name__ == "__main__":
-    X = np.array(([1, -2], [-3, 4], [5, 6]))
-    # print(X)
-    # w = np.array(([-1], [-2], [-3]))
-    # node1 = Multiply()
-    # print(node1.forward(X, w))
-    # dz = np.array([0, 1]).reshape((2, 1))
-    # print(node1.backward(dz))
-    node1 = Softmax()
-    print(node1.forward(X))
-    #true_y = np.array([0.09, 0.2447, 1]).reshape((3, 1))
-    # print(node1.backward(true_y))
+    X = np.random.uniform(0, 1, (2, 2))
+    print(X)
+    # w1 = np.random.uniform(0 ,1, (6, 16))
+    # mul1 = Multiply()
+    # relu = ReLu()
+    # mul2 = Multiply()
+    # smax = Softmax()
 
-    # node1 = ReLu()
-    # print(node1.forward(X))
-    # dz = np.ones(X.shape)
-    # print(node1.backward(dz))
+    # out1 = mul1.forward(X, w1)
+    # out2 = relu.forward(out1)
+    # w2 = np.random.uniform(0 ,1, (17, 5))
+    # out3 = mul2.forward(out2, w2)  
+    # out4 = smax.forward(out3)
+    # print(out4[1])
+
+
+    ###########
+    output_true = np.ones(out4.shape)
+    d_out4 = smax.backward(output_true)
+    d_out3, d_w2 = mul2.backward(d_out4)
+    d_out2 = relu.backward(d_out3)
+    d_out1, d_w1 = mul1.backward(d_out2)
+    # print(d_out1.shape)
