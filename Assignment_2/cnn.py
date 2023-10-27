e40230c1d8785371f71e9af847a2f5115bdc327f
thien <68079602+trongthien1603@users.noreply.github.com>
2023-10-27 13:39:11 -0400

    Updated input channels and sizes
    These changes work for both datasets

--- Assignment_2/cnn.py
+++ Assignment_2/cnn.py
@@ -1,56 +1,77 @@
+import numpy as np
+import random
 import copy
+import matplotlib.pyplot as plt
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as function
-from torchvision import datasets, transforms
+from torchvision import datasets
+import torchvision.transforms as transforms
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from sklearn.preprocessing import StandardScaler
 
+class DynamicCNN(nn.Module):
+    def __init__(self, num_classes, input_channels, input_size):
+        super(DynamicCNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=32, kernel_size=3, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.relu2 = nn.ReLU()
+
+        # Calculate the size of the fully connected layer
+        self.fc_input_size = 64 * (input_size // 4) * (input_size // 4)
+
+        self.fc1 = nn.Linear(self.fc_input_size, 128)
+        self.relu3 = nn.ReLU()
+        self.fc2 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.relu2(x)
+        x = self.pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.relu3(x)
+        x = self.fc2(x)
+        return x
+
 class CNN:
-    @staticmethod
-    def create():
-        model = nn.Sequential(
-            nn.Conv2d(1, 6, 5, padding=2),
-            nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),
-            
-            nn.Conv2d(6, 16, 5, padding=0),
-            nn.ReLU(),
-            nn.MaxPool2d(2, stride=2),
-            
-            nn.Flatten(),
-            nn.Linear(400, 128),  # Set to 128 units
-            nn.ReLU(),
-            nn.Linear(128, 128),  # Set to 128 units
-            nn.ReLU(),
-            nn.Linear(128, 10)
-        )
-        return model
+    def __init__(self, num_classes, input_channels, input_size):
+        self.num_classes = num_classes
+        self.input_channels = input_channels
+        self.input_size = input_size
 
     @staticmethod
-    def validate(model, data):
+    def validate(model, data, device):
         correct = 0
-        for i, (images, labels) in enumerate(data):
-            images = images.to("cpu")  # Move images to CPU
-            x = model(images)
-            value, pred = torch.max(x, 1)
-            correct += (pred == labels).sum().item()
+        with torch.no_grad():
+            for images, labels in data:
+                images = images.to(device)
+                labels = labels.to(device)
+                x = model(images)
+                value, pred = torch.max(x, 1)
+                correct += (pred == labels).sum().item()
         accuracy = 100 * correct / len(data.dataset)
         return accuracy
 
-    @staticmethod
-    def train(trainloader, testloader, num_epoch=3, lr=1e-3, device="cpu"):
+    def train(self, trainloader, testloader, num_epochs=5, lr=0.001, device="cpu"):
         accuracies = []
-        cnn = CNN.create().to(device)
+        cnn = DynamicCNN(self.num_classes, self.input_channels, self.input_size).to(device)
         cec = nn.CrossEntropyLoss()
         optimizer = optim.Adam(cnn.parameters(), lr=lr)
         max_accuracy = 0
         best_model = None
 
-        for epoch in range(num_epoch):
-            for i, (images, labels) in enumerate(trainloader):
+        for epoch in range(num_epochs):
+            cnn.train()
+            for images, labels in trainloader:
                 images = images.to(device)
                 labels = labels.to(device)
                 optimizer.zero_grad()
@@ -58,17 +79,30 @@ class CNN:
                 loss = cec(pred, labels)
                 loss.backward()
                 optimizer.step()
-            accuracy = float(CNN.validate(cnn, testloader))
+
+            cnn.eval()
+            accuracy = self.validate(cnn, testloader, device)
             accuracies.append(accuracy)
+
             if accuracy > max_accuracy:
                 best_model = copy.deepcopy(cnn)
                 max_accuracy = accuracy
-            print("Epoch", epoch + 1, ": Accuracy ", accuracy, "%")
-        plt.plot(accuracies)
 
-        return best_model
+            print("Epoch", epoch + 1, ": Accuracy", accuracy, "%")
+
+        return accuracies, best_model
+
     
     
-fashion_cnn = CNN.train(trainloader_fashion, testloader_fashion, 20, device = device)
+cnn_cifar = CNN(10, 3, 32)
+accuracies_cifar, best_model_cifar = cnn_cifar.train(trainloader_cifar, testloader_cifar, num_epochs=5, lr=0.001, device="cpu")
 
-cifar_cnn = CNN.train(trainloader_cifar, testloader_cifar, 20, device = device)
+cnn_fashion = CNN(10, 1, 28)
+accuracies_fashion, best_model_fashion = cnn_fashion.train(trainloader_fashion, testloader_fashion, num_epochs=5, lr=0.001, device="cpu")
+
+plt.plot(accuracies_cifar, label="CIFAR dataset")
+plt.plot(accuracies_fashion, label="Fashion dataset")
+plt.legend()
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.title("CNN models")
