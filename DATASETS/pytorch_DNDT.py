4df2e7413a08a2d560361104e09574e1f615ef77
AlexHal <jonathanhalimi12@gmail.com>
2023-12-07 19:07:40 -0500

    added normalization and test/train options

--- DATASETS/pytorch_DNDT.py
+++ DATASETS/pytorch_DNDT.py
@@ -10,6 +10,8 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+
 
 def normalize_dataframe_min_max(df):
     scaler = MinMaxScaler()
@@ -47,7 +49,9 @@ def encode_column(column):
 #TODO: add unimportant features
 
 
-def build_test_DNDT(datafile, output_column_name, num_features, num_cuts, seed=None, exclude_features=[]):
+def build_test_DNDT(datafile, output_column_name, num_features, num_cuts, seed=None, exclude_features=[], normalized=False, test_train_split=False):
+    if test_train_split:
+        return build_test_train_DNDT(datafile, output_column_name, num_features, num_cuts, seed, exclude_features, normalized)
     # Set the random seed for PyTorch
     if seed is not None:
         torch.manual_seed(seed)
@@ -55,15 +59,14 @@ def build_test_DNDT(datafile, output_column_name, num_features, num_cuts, seed=N
     # Read the CSV file with headers
     df = pd.read_csv(datafile)
 
-    
 
     # Remove unimportant features
     df = df.drop(columns=exclude_features)
 
     # Encode categorical columns
     df = df.apply(encode_column)
-
-    df = normalize_dataframe_min_max(df) #testing idea
+    if normalized:
+        df = normalize_dataframe_min_max(df) #testing idea
 
     # Extract features and target variable
     feature_columns = df.columns.difference([output_column_name])
@@ -114,5 +117,83 @@ def build_test_DNDT(datafile, output_column_name, num_features, num_cuts, seed=N
 
     y_pred = nn_decision_tree(x_tensor, cut_points_list, leaf_score, temperature=0.1)
     accuracy_score(y_tensor.argmax(1), y_pred.argmax(1))
-    #print(accuracy_score(y_tensor.argmax(1), y_pred.argmax(1)))
     return accuracy_score(y_tensor.argmax(1), y_pred.argmax(1))
+
+
+
+
+
+def build_test_train_DNDT(datafile, output_column_name, num_features, num_cuts, seed=None, exclude_features=[], normalized=False):
+    
+     # Set the random seed for PyTorch
+    if seed is not None:
+        torch.manual_seed(seed)
+
+    # Read the CSV file with headers
+    df = pd.read_csv(datafile)
+
+
+    # Remove unimportant features
+    df = df.drop(columns=exclude_features)
+
+    # Encode categorical columns
+    df = df.apply(encode_column)
+    if normalized:
+        df = normalize_dataframe_min_max(df) #testing idea
+
+    # Extract features and target variable
+    feature_columns = df.columns.difference([output_column_name])
+    X = df[feature_columns].to_numpy()
+    y = df[output_column_name].to_numpy()
+
+    # Convert the species labels to integers
+    species_to_int = {species: idx for idx, species in enumerate(np.unique(y))}
+    y_int = np.array([species_to_int[eval] for eval in y])
+
+    # Optionally, convert the integer labels to one-hot encoding
+    def one_hot_encode(labels, num_classes):
+        return np.eye(num_classes)[labels]
+    
+    num_classes = len(species_to_int)
+    y_one_hot = one_hot_encode(y_int, num_classes)
+
+
+    num_cut = [1]*num_cuts #num_features  
+    num_leaf = np.prod(np.array(num_cut) + 1)
+    #d = X.shape[1]
+    #print(X.shape, y.shape, d, num_cut, num_leaf, num_classes)
+
+    # Initialize variables
+    cut_points_list = [torch.nn.Parameter(torch.rand(i)) for i in num_cut]
+    leaf_score = torch.nn.Parameter(torch.rand(num_leaf, num_classes))
+
+    # Define loss and optimizer
+    optimizer = torch.optim.Adam([*cut_points_list, leaf_score], lr=0.1)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    loss_function = torch.nn.CrossEntropyLoss()
+
+    # Train-test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y_int, test_size=0.2, random_state=seed)
+    # Convert to PyTorch tensors
+    x_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+    y_train_tensor = torch.tensor(y_train, dtype=torch.int64)
+    x_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+    y_test_tensor = torch.tensor(y_test, dtype=torch.int64)
+
+
+
+    # Training loop
+    for i in range(1000):
+        optimizer.zero_grad()
+        y_pred = nn_decision_tree(x_train_tensor, cut_points_list, leaf_score, temperature=0.1)
+        loss = loss_fn(y_pred, y_train_tensor)
+        loss.backward()
+        optimizer.step()
+    
+    # Evaluate
+    with torch.no_grad():
+        y_pred_test = nn_decision_tree(x_test_tensor, cut_points_list, leaf_score, temperature=0.1)
+        accuracy = accuracy_score(y_test_tensor, y_pred_test.argmax(1))
+
+    return accuracy
+
